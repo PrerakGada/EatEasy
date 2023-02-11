f1a361e8e4678fa0ac7145a2841b6798e005a976
MightyM17 <mithilbavishi17@gmail.com>
2023-02-11 20:19:56 +0530

    Rating

--- lib/screens/Customer/customer_home.dart
+++ lib/screens/Customer/customer_home.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:eat_easy/screens/Customer/SideBar.dart';
+import 'package:eat_easy/screens/Provider/InfoPage.dart';
 import 'package:flutter/material.dart';
 
 import '../../Theme/app_colors.dart';
@@ -31,51 +32,54 @@ class CustomerHome extends StatelessWidget {
               alignment: Alignment.topCenter,
               child: Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20),
-                          elevation: 10,
-                          child: const CircleAvatar(
-                            radius: 22,
-                            backgroundImage: NetworkImage(
-                              'https://source.unsplash.com/50x50/?portrait',
+                  Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          GestureDetector(
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20),
+                              elevation: 10,
+                              child: const CircleAvatar(
+                                radius: 22,
+                                backgroundImage: NetworkImage(
+                                  'https://source.unsplash.com/50x50/?portrait',
+                                ),
+                              ),
+                            ),
+                            onTap: () {
+                              _scaffoldState.currentState?.openDrawer();
+                            },
+                          ),
+                          Container(
+                            width: 300,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(25),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey.withOpacity(0.6),
+                                    offset: const Offset(5.0, 5.0),
+                                    blurRadius: 10.0,
+                                    spreadRadius: 2.0), //BoxShadow
+                              ],
+                            ),
+                            child: const Align(
+                              alignment: Alignment.centerLeft,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Search',
+                                  hintStyle: TextStyle(color: Colors.black),
+                                  contentPadding: EdgeInsets.only(left: 20),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                        onTap: () {
-                          _scaffoldState.currentState?.openDrawer();
-                        },
+                        ],
                       ),
-                      Container(
-                        width: 300,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(25),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.withOpacity(0.6),
-                                offset: const Offset(5.0, 5.0),
-                                blurRadius: 10.0,
-                                spreadRadius: 2.0), //BoxShadow
-                          ],
-                        ),
-                        child: const Align(
-                          alignment: Alignment.centerLeft,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Search',
-                              hintStyle: TextStyle(color: Colors.black),
-                              contentPadding: EdgeInsets.only(left: 20),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
                   ),
                   const Spacer(flex: 5),
                   Container(
@@ -141,75 +145,80 @@ class CustomerHome extends StatelessWidget {
                             items: [1, 2, 3, 4, 5].map((i) {
                               return Builder(
                                 builder: (BuildContext context) {
-                                  return Container(
-                                    height: 170,
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 5.0),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      color: const Color(0xffaaaaaa),
-                                      image: const DecorationImage(
-                                          image: NetworkImage(
-                                              "https://picsum.photos/300"),
-                                          fit: BoxFit.fill),
-                                    ),
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Container(
-                                          height: 100,
-                                          width: double.infinity,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(15),
-                                            color: const Color(0xFFFFFF)
-                                                .withOpacity(0.7),
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => InfoPage()));
+                                      },
+                                    child: Container(
+                                      height: 200,
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 5.0),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(15),
+                                        color: const Color(0xffaaaaaa),
+                                        image: const DecorationImage(
+                                            image: NetworkImage(
+                                                "https://content.jdmagicbox.com/comp/mumbai/46/022p5463446/catalogue/new-shahi-family-restaurant-mira-road-thane-tandoori-restaurants-c7lj66kagt.jpg?clr="),
+                                            fit: BoxFit.fill),
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Container(
+                                            height: 100,
+                                            width: double.infinity,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                              BorderRadius.circular(15),
+                                              color: const Color(0xFFFFFF)
+                                                  .withOpacity(0.7),
+                                            ),
+                                            child: Column(
+                                                crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                                mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                                children: [
+                                                  const Text(
+                                                    "New Shahi Restaurant",
+                                                    style: TextStyle(
+                                                        fontSize: 18,
+                                                        color: AppColors.black,
+                                                        fontWeight:
+                                                        FontWeight.bold),
+                                                  ),
+                                                  Text(
+                                                    "Opp old petrol pump Thane-401107",
+                                                    style: TextStyle(
+                                                        color: AppColors.black
+                                                            .withOpacity(0.5),
+                                                        fontSize: 11,
+                                                        fontWeight:
+                                                        FontWeight.bold),
+                                                  ),
+                                                  const Row(
+                                                    mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                    children: [
+                                                      Text(
+                                                        "4.5/5",
+                                                        style: TextStyle(
+                                                            color:
+                                                            AppColors.black,
+                                                            fontSize: 11,
+                                                            fontWeight:
+                                                            FontWeight.bold),
+                                                      ),
+                                                      Icon(
+                                                        Icons.star,
+                                                        color: Colors.yellow,
+                                                      ),
+                                                    ],
+                                                  )
+                                                ]),
                                           ),
-                                          child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.center,
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                const Text(
-                                                  "New Shahi Restaurant",
-                                                  style: TextStyle(
-                                                      fontSize: 18,
-                                                      color: AppColors.black,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                Text(
-                                                  "Opp old petrol pump Thane-401107",
-                                                  style: TextStyle(
-                                                      color: AppColors.black
-                                                          .withOpacity(0.5),
-                                                      fontSize: 11,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ),
-                                                const Row(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                  children: [
-                                                    Text(
-                                                      "4.5/5",
-                                                      style: TextStyle(
-                                                          color:
-                                                              AppColors.black,
-                                                          fontSize: 11,
-                                                          fontWeight:
-                                                              FontWeight.bold),
-                                                    ),
-                                                    Icon(
-                                                      Icons.star,
-                                                      color: Colors.yellow,
-                                                    ),
-                                                  ],
-                                                )
-                                              ]),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
                                   );
                                 },
--- lib/screens/Provider/InfoPage.dart
+++ lib/screens/Provider/InfoPage.dart
@@ -13,58 +13,50 @@ class _InfoPageState extends State<InfoPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body:
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network('https://cdn.vox-cdn.com/thumbor/ovQdcnuMVAYVQ-gSfOHDzCCs-vU=/0x0:6192x4128/1200x800/filters:focal(2601x1569:3591x2559)/cdn.vox-cdn.com/uploads/chorus_image/image/70073834/GettyImages_1331161454.0.jpg'),
-          SizedBox(height: 20,),
-          Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text('McDonald\'s', style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),),
-          ),
           Padding(
             padding: EdgeInsets.only(left: 15),
-            child: Row(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  "4.5/5",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
+                Image.network('https://cdn.vox-cdn.com/thumbor/ovQdcnuMVAYVQ-gSfOHDzCCs-vU=/0x0:6192x4128/1200x800/filters:focal(2601x1569:3591x2559)/cdn.vox-cdn.com/uploads/chorus_image/image/70073834/GettyImages_1331161454.0.jpg'),
+                SizedBox(height: 20,),
+                Text('McDonald\'s', style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),),
+                Row(
+                    children: [
+                      Text(
+                        "4.5/5",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
                       ),
+                      Icon(Icons.star, color: Colors.yellow,),
+                    ],
+                  ),
+                SizedBox(height: 30,),
+                Text('McDonald\'s Corporation is an American multinational fast food chain, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States', style: TextStyle(color: Colors.black, fontSize: 15),),
+                SizedBox(height: 20,),
+                Text('Location', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                //TODO: Add Maps location here
+                SizedBox(height: 20,),
+                Text('Reviews', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                SizedBox(height: 20,),
+                Text('Must visit', style: TextStyle(color: Colors.black, fontSize: 18),),
+                Text('User #1', style: TextStyle(color: Colors.black, fontSize: 15),),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star, color: Colors.yellow, size: 14.0),
+                    Icon(Icons.star, color: Colors.yellow, size: 14.0),
+                    Icon(Icons.star, color: Colors.yellow, size: 14.0),
+                    Icon(Icons.star, color: Colors.yellow, size: 14.0),
+                    Icon(Icons.star_border, color: Colors.grey, size: 14.0),
+                  ],
                 ),
-                Icon(Icons.star, color: Colors.yellow,),
+                SizedBox(height: 8,),
+                Text('Good quick service. Need to keep a check on the hygiene. Tables need to be cleaned a little faster considering the table turnovers. The tray underlines need to be changed and not reused again. The food was alright. Fries a little under seasoned. The burger bun was crumbly. Coke was no ice but water was added a little extra to it.', style: TextStyle(color: Colors.black, fontSize: 15),),
               ],
             ),
           ),
-          SizedBox(height: 30,),
-          Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text('McDonald\'s Corporation is an American multinational fast food chain, founded in 1940 as a restaurant operated by Richard and Maurice McDonald, in San Bernardino, California, United States', style: TextStyle(color: Colors.black, fontSize: 15),),
-          ),
-          SizedBox(height: 20,),
-          Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text('Location', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
-          ),
-          //TODO: Add Maps location here
-          SizedBox(height: 20,),
-          Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text('Reviews', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
-          ),
-          SizedBox(height: 20,),
-          Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text('Must visit', style: TextStyle(color: Colors.black, fontSize: 18),),
-          ),
-          SizedBox(height: 8,),
-          Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text('Good quick service. Need to keep a check on the hygiene. Tables need to be cleaned a little faster considering the table turnovers. The tray underlines need to be changed and not reused again. The food was alright. Fries a little under seasoned. The burger bun was crumbly. Coke was no ice but water was added a little extra to it.', style: TextStyle(color: Colors.black, fontSize: 15),),
-          ),
-        ],
-      ),
     );
   }
 }
