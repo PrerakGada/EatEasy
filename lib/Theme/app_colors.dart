a496784dd85d9ef8e3b938577a2e983a2d956df4
MightyM17 <mithilbavishi17@gmail.com>
2023-02-11 19:09:57 +0530

    Change primary colors

--- lib/Theme/app_colors.dart
+++ lib/Theme/app_colors.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
 abstract class AppColors {
-  static const primary = Color(0xff024b1d);
+  static const primary = Color(0xff007845);
   static const primaryAccent = Color(0xff06e357);
-  static const secondary = Color(0xfffba839);
+  static const secondary = Color(0xffffac1c);
   static const black = Color(0xFF121212);
   static const greyDark = Color(0xFF212121);
   static const grey = Color(0xFF535353);
