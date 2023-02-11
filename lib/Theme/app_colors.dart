d0df7ef4423087c027d06d3960faadfde8adae3f
Mithil Bavishi <bavishimithil@gmail.com>
2023-02-11 13:15:02 +0530

    Update colors

--- lib/Theme/app_colors.dart
+++ lib/Theme/app_colors.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
 abstract class AppColors {
-  static const primary = Color(0xff7b00ff);
-  static const primaryAccent = Color(0xffdbabff);
-  static const secondary = Color(0xff002962);
+  static const primary = Color(0xff024b1d);
+  static const primaryAccent = Color(0xff06e357);
+  static const secondary = Color(0xfffba839);
   static const black = Color(0xFF121212);
   static const greyDark = Color(0xFF212121);
   static const grey = Color(0xFF535353);
