e0c8dbb2595a12186bd6c198e4693a309123a40e
Siddesh <siddushetty30@gmail.com>
2023-02-11 19:40:22 +0530

    Pick Image Added

--- lib/screens/provider_verification.dart
+++ lib/screens/provider_verification.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
 import 'package:eat_easy/repositories/query_repo.dart';
 import 'package:eat_easy/screens/Admin/admin_screen.dart';
 import 'package:eat_easy/widgets/LabeledTextFormField.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 class ProviderVerification extends StatefulWidget {
@@ -22,6 +26,23 @@ class _ProviderVerificationState extends State<ProviderVerification> {
   final TextEditingController _fssai_noController = TextEditingController();
   final TextEditingController _gst_noController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  File? demo;
+
+  void onPickImageButtonClicked() async {
+    final tempImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (tempImage == null) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('An error occurred. Failed to pick image!'),
+      ));
+      return;
+    }
+
+    setState(() {
+      demo = File(tempImage.path);
+    });
+  }
 
   @override
   void dispose() {
@@ -49,51 +70,87 @@ class _ProviderVerificationState extends State<ProviderVerification> {
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-              LabeledTextFormField(
-                  controller: _usernameController,
-                  title: 'Username',
-                  hintTitle: 'Enter your name'),
-              Spacer(),
-              LabeledTextFormField(
-                  controller: _emailController,
-                  title: 'Email',
-                  hintTitle: 'Enter your Email'),
-              Spacer(),
-              LabeledTextFormField(
-                  controller: _mobileController,
-                  title: 'Mobile',
-                  hintTitle: 'Enter your Mobile Number'),
-              Spacer(),
-              LabeledTextFormField(
-                  controller: _descriptionController,
-                  title: 'Description',
-                  hintTitle: 'Enter your Description'),
-              Spacer(),
-              LabeledTextFormField(
-                  controller: _fssai_noController,
-                  title: 'FSSAI No.',
-                  hintTitle: 'Enter your FSSAI no.'),
-              Spacer(),
-              LabeledTextFormField(
-                  controller: _gst_noController,
-                  title: 'GST No.',
-                  hintTitle: 'Enter your GST no.'),
-              Spacer(flex: 3),
-              ElevatedButton(
-                onPressed: () async {
-                  if (await QueryRepo().submitProviderVerification(
-                    _usernameController.text,
-                    _emailController.text,
-                    _mobileController.text,
-                    _descriptionController.text,
-                    _fssai_noController.text,
-                    _gst_noController.text,
-                  )) {
-                    Navigator.pushNamed(context, AdminDashBoard.id);
-                  }
-                },
-                child: const Text("Submit"),
-              ),
+              (demo != null)
+                  ? Align(
+                      alignment: Alignment.center,
+                      child: DottedBorder(
+                        child: Image.file(
+                          demo!.absolute,
+                          height: 200,
+                          width: 280,
+                          scale: 2,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    )
+                  : Align(
+                      alignment: Alignment.center,
+                      child: DottedBorder(
+                        child: MaterialButton(
+                          onPressed: () async {
+                            // if (kIsWeb) {
+                            //   startweb();
+                            // } else {
+                            onPickImageButtonClicked();
+                          },
+                          child: const Icon(Icons.image),
+                        ),
+                      ),
+                    ),
+              // SizedBox(
+              //   height: 10,
+              // ),
+              Expanded(
+                child: Column(
+                  children: [
+                    LabeledTextFormField(
+                        controller: _usernameController,
+                        title: 'Username',
+                        hintTitle: 'Enter your name'),
+                    Spacer(),
+                    LabeledTextFormField(
+                        controller: _emailController,
+                        title: 'Email',
+                        hintTitle: 'Enter your Email'),
+                    Spacer(),
+                    LabeledTextFormField(
+                        controller: _mobileController,
+                        title: 'Mobile',
+                        hintTitle: 'Enter your Mobile Number'),
+                    Spacer(),
+                    LabeledTextFormField(
+                        controller: _descriptionController,
+                        title: 'Description',
+                        hintTitle: 'Enter your Description'),
+                    Spacer(),
+                    LabeledTextFormField(
+                        controller: _fssai_noController,
+                        title: 'FSSAI No.',
+                        hintTitle: 'Enter your FSSAI no.'),
+                    Spacer(),
+                    LabeledTextFormField(
+                        controller: _gst_noController,
+                        title: 'GST No.',
+                        hintTitle: 'Enter your GST no.'),
+                    Spacer(flex: 3),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (await QueryRepo().submitProviderVerification(
+                          _usernameController.text,
+                          _emailController.text,
+                          _mobileController.text,
+                          _descriptionController.text,
+                          _fssai_noController.text,
+                          _gst_noController.text,
+                        )) {
+                          Navigator.pushNamed(context, AdminDashBoard.id);
+                        }
+                      },
+                      child: const Text("Submit"),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
