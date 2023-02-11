0a19242abbe7b7148a10d2088827c799491457ce
Prerak Gada <prerakgada07@gmail.com>
2023-02-12 01:15:21 +0530

    Fixed Profile navigation and scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eat_easy/screens/Admin/admin_screen.dart';
 import 'package:eat_easy/screens/Customer/customer_home.dart';
+import 'package:eat_easy/screens/Customer/user_profile_screen.dart';
 import 'package:eat_easy/screens/onboarding/login_screen.dart';
 import 'package:eat_easy/screens/Provider/provider_verification.dart';
 import 'package:eat_easy/screens/onboarding/signUp_screen.dart';
@@ -44,9 +45,9 @@ class MyApp extends StatelessWidget {
         darkTheme: dark_theme(),
         themeMode: ThemeMode.light,
         debugShowCheckedModeBanner: false,
-        initialRoute: SplashScreen.id,
+        // initialRoute: SplashScreen.id,
         //initialRoute: AdminDashBoard.id,
-        //initialRoute: CustomerHome.id,
+        initialRoute: CustomerHome.id,
         // initialRoute: ProviderVerification.id,
         // initialRoute: AdminDashBoard.id,
         // initialRoute: CustomerHome.id,
@@ -58,6 +59,7 @@ class MyApp extends StatelessWidget {
           CustomerHome.id: (context) => CustomerHome(),
           ProviderVerification.id: (context) => ProviderVerification(),
           AdminDashBoard.id: (context) => AdminDashBoard(),
+          ProfileScreen.id: (context) => ProfileScreen(),
         },
       ),
     );
